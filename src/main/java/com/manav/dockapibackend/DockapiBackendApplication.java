9d78906ba3d9d82500d008489af491030bd22886
manav <manavkhadka2004@gmail.com>
2025-01-20 10:18:27 +0530

    [Created] project created 🏗️

---
+++ src/main/java/com/manav/dockapibackend/DockapiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manav.dockapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockapiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockapiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manav/dockapibackend/DockapiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manav.dockapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockapiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
