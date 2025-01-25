043ecb5fac087bc597ee72f594e63274197030b1
manav <manavkhadka2004@gmail.com>
2025-01-26 02:00:19 +0530

    main server created

---
+++ dockapi-main-server/src/main/java/com/manav/dockapimainserver/DockapiMainServerApplication.java
@@ -0,0 +1,13 @@
+package com.manav.dockapimainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockapiMainServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockapiMainServerApplication.class, args);
+	}
+
+}
